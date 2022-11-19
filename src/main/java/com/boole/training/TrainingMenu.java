b8d50a7be620968f9a60b3df5fe49cf206f4bc9e
boole <samarthupadhya2309@gmail.com>
2022-11-19 08:55:28 -0500

    training menu and parameter database for cost function

--- src/main/java/com/boole/training/TrainingMenu.java
+++ src/main/java/com/boole/training/TrainingMenu.java
@@ -1,22 +1,17 @@
-package com.boole;
+package com.boole.training;
 
-import com.boole.drawing.DrawingBoard;
-import com.boole.training.TrainingMenu;
+import com.boole.style;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Home extends JPanel implements ActionListener {
+public class TrainingMenu extends JPanel implements ActionListener {
 
     private final JFrame window;
     private final Graphics2D graphics;
 
-    public static void main(String[] args) {
-        new Home();
-    }
-
-    public Home() {
+    public TrainingMenu() {
 
         setLayout(null);
         setFocusable(true);
@@ -46,17 +41,15 @@ public class Home extends JPanel implements ActionListener {
     }
 
     private void addComponents() {
-        JLabel chooseText = new JLabel("Select which action you would like to perform:");
+        JLabel chooseText = new JLabel("<html><h1 align=\"center\">Select which training action you would like <br>to perform:</h1></html>");
         chooseText.setName("label1");
         chooseText.setFont(style.bigFont);
         chooseText.setForeground(style.lightText);
         chooseText.setVisible(true);
-        chooseText.setBounds(80, 30, 800, 200);
+        chooseText.setBounds(88, 26, 800, 200);
 
         JButton trainButton = new JButton();
-        trainButton.setText(centerFormatText("Train the neural network with random MNIST Database samples"));
-        trainButton.setHorizontalAlignment(SwingConstants.CENTER);
-        trainButton.setVerticalAlignment(SwingConstants.CENTER);
+        trainButton.setText(centerFormatText("Train the neural network with randomly parsed samples"));
         trainButton.setFont(style.normalFont);
         trainButton.setName("train");
         trainButton.setFocusable(false);
@@ -65,9 +58,7 @@ public class Home extends JPanel implements ActionListener {
         trainButton.setBounds(100, 220, 200, 160);
         trainButton.setVisible(true);
         JButton detectButton = new JButton();
-        detectButton.setText(centerFormatText("Detect your hand-written digits using the trained or untrained neural network"));
-        detectButton.setHorizontalAlignment(SwingConstants.CENTER);
-        detectButton.setVerticalAlignment(SwingConstants.CENTER);
+        detectButton.setText(centerFormatText("Reset all training progress achieved until this point"));
         detectButton.setFont(style.normalFont);
         detectButton.setName("detect");
         detectButton.setFocusable(false);
@@ -92,19 +83,18 @@ public class Home extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         String command = stripHTMLTags(actionEvent.getActionCommand());
-        if (command.startsWith("train")) {
+        if (command.startsWith("reset")) {
             // close this window
             this.window.setVisible(false);
             this.window.dispose();
 
-            TrainingMenu menu = new TrainingMenu();
-        } else if(command.startsWith("detect")) {
+            // TODO: Randomize the paramater values in database/params.json
+        } else if(command.startsWith("train")) {
             // close this window
             this.window.setVisible(false);
             this.window.dispose();
 
-            // create and open drawing board
-            DrawingBoard board = new DrawingBoard();
+            // TODO: Run the Neural Network through all parsed training sample data
         }
     }
 
