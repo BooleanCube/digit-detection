0c726b8f917570cd843890981578c04f964552e5
boole <samarthupadhya2309@gmail.com>
2022-12-05 22:18:52 -0500

    calculus equations, functions and models

--- src/main/java/com/boole/Calculator.java
+++ src/main/java/com/boole/Calculator.java
@@ -2,12 +2,33 @@ package com.boole;
 
 import com.boole.network.models.Node;
 
+/**
+ * Static calculator class with all the relevant calculus equations and models used.
+ * ----
+ * Table of Elements =>
+ * C = Cost
+ * L = Layer
+ * w = weight
+ * b = bias
+ * a = activation (previous/current)
+ * z = sum (wa+b)
+ * y = goal
+ * σ = sigmoid function symbol
+ * ∂ = partial differential symbol
+ */
 public class Calculator {
 
     private final static double sigmoidStretch = 1.5;
 
+    // 1(1+e^(-x))^(-1)
     public static double sigmoid(double x) {
         return 1/(1+Math.pow(Math.E-sigmoidStretch, -x));
+    }
+
+    // σ(x)(1-σ(x))
+    // https://towardsdatascience.com/derivative-of-the-sigmoid-function-536880cf918e
+    public static double sigmoidPrime(double x) {
+        return sigmoid(x)*(1-sigmoid(x));
     }
 
     public static double calculateCost(Node[] output, double[] exact) {
@@ -23,6 +44,67 @@ public class Calculator {
         for(int i=1; i<arr.length; i++)
             if(arr[i].getActivation()>arr[largest].getActivation()) largest = i;
         return largest;
+    }
+
+    /**
+     * ∂C/∂wL => direction of the steepest descent with respect to the weight
+     */
+    public static double weightPartialDiff(double currActivation, double prevActivation, double weight, double bias, double goal) {
+        double sum = weight * prevActivation + bias;
+        // Chain Rule ===>
+        // ∂C/∂wL = ∂C/∂aL * ∂aL/∂zL * ∂zL/∂wL
+        // Derivative of cost with respect to weight =
+        // Derivative of cost with respect to activation * Derivative of activation with respect to sum * Derivative of sum with respect to weight
+        return costDiffActivation(currActivation, goal) * activationDiffSum(sum) * sumDiffWeight(prevActivation);
+    }
+
+    /**
+     * ∂C/∂bL => direction of the steepest descent with respect to the bias
+     */
+    public static double biasPartialDiff(double currActivation, double prevActivation, double weight, double bias, double goal) {
+        double sum = weight * prevActivation + bias;
+        // Chain Rule ===>
+        // ∂C/∂bL = ∂C/∂aL * ∂aL/∂zL * ∂zL/∂bL
+        // Derivative of cost with respect to weight =
+        // Derivative of cost with respect to activation * Derivative of activation with respect to sum * Derivative of sum with respect to bias
+        return costDiffActivation(currActivation, goal) * activationDiffSum(sum) * sumDiffBias();
+    }
+
+    /**
+     * ∂C/∂aL => direction of the steepest descent with respect to the previous activation
+     */
+    public static double activationPartialDiff(double currActivation, double prevActivation, double weight, double bias, double goal) {
+        double sum = weight * prevActivation + bias;
+        // Chain Rule ===>
+        // ∂C/∂aL = ∂C/∂aL * ∂aL/∂zL * ∂zL/∂aL
+        // Derivative of cost with respect to previous activation =
+        // Derivative of cost with respect to activation * Derivative of activation with respect to sum * Derivative of sum with respect to previous activation
+        return costDiffActivation(currActivation, goal) * activationDiffSum(sum) * sumDiffActivation(weight);
+    }
+
+    // ∂C/∂aL (Derivative of cost with respect to activation)
+    public static double costDiffActivation(double activation, double goal) {
+        return 2*(activation+goal);
+    }
+
+    // ∂aL/∂zL (Derivative of activation with respect to sum)
+    public static double activationDiffSum(double sum) {
+        return sigmoidPrime(sum);
+    }
+
+    // ∂zL/∂wL (Derivative of sum with respect to weight)
+    public static double sumDiffWeight(double activation) {
+        return activation;
+    }
+
+    // ∂zL/∂bL (Derivative of sum with respect to bias)
+    public static double sumDiffBias() {
+        return 1;
+    }
+
+    // ∂zL/∂aL (Derivative of sum with respect to previous activation)
+    public static double sumDiffActivation(double weight) {
+        return weight;
     }
 
 }
